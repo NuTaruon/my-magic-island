61e6180b591540d09ad3eda8a090873dc661f77c
NuTaruon <dkgkrukov21@gmail.com>
2026-02-23 21:51:38 +0300

    edit Animal

--- src/main/java/animals/Animal.java
+++ src/main/java/animals/Animal.java
@@ -1,15 +1,20 @@
 package animals;
 
 public abstract class Animal {
-    private int weight;
-    private int speed;
+    private final int weight;
+    private final int speed;
+    private final int maxSaturation;
     private int saturation;
+    private boolean alive = true;
 
-    public Animal(int saturation, int speed, int weight) {
+    public Animal(int maxSaturation, int saturation, int speed, int weight) {
+        this.maxSaturation = maxSaturation;
         this.saturation = saturation;
         this.speed = speed;
         this.weight = weight;
     }
+
+    public int getMaxSaturation() {return maxSaturation;}
 
     public int getWeight() {
         return weight;
@@ -21,6 +26,18 @@ public abstract class Animal {
 
     public int getSaturation() {
         return saturation;
+    }
+
+    public void setSaturation(int saturation) {
+        this.saturation = saturation;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
     }
 
     abstract void eat();
