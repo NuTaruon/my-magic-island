352829fc22ff90e33aa30228c2102bab1ac3c778
NuTaruon <dkgkrukov21@gmail.com>
2026-02-23 21:51:59 +0300

    class Herbivores

---
+++ src/main/java/animals/herbivores/Herbivores.java
@@ -0,0 +1,10 @@
+package animals.herbivores;
+
+import animals.Animal;
+
+public abstract class Herbivores extends Animal {
+
+    public Herbivores(int maxSaturation, int saturation, int speed, int weight) {
+        super(maxSaturation, saturation, speed, weight);
+    }
+}
