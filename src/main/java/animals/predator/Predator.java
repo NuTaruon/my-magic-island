bb4cb671c937aaf927f46508787b7043644da6aa
NuTaruon <dkgkrukov21@gmail.com>
2026-02-23 21:52:11 +0300

    class Predator

---
+++ src/main/java/animals/predator/Predator.java
@@ -0,0 +1,11 @@
+package animals.predator;
+
+import animals.Animal;
+
+public abstract class Predator extends Animal {
+
+    public Predator(int maxSaturation, int saturation, int speed, int weight) {
+        super(maxSaturation, saturation, speed, weight);
+    }
+
+}
